4f2b735b781649636217730e3f82362bbcb008b3
Ayotunde <ayotundeodejayi@yahoo.com>
2016-03-01 04:55:03 -0500

    Update Myo_Fann.cpp

--- Myo_Fann.cpp
+++ Myo_Fann.cpp
@@ -21,14 +21,19 @@ namespace Myo_Fann
 
 		// Trains w/ entire dataset or individual data
 		if (TerminatorMyo_Fann::toCalibrate){
+			std::cout << "Calibrating logged user data... \n\n";
 			fann_train_on_file(ann, "C:\\Users\\Ayotunde\\Documents\\Visual Studio 2013\\Projects\\fann-master\\fann-master\\examples\\MyoCal.data", max_epochs, epochs_between_reports, desired_error);
-			std::cout << "Calibrating with user data! \n\n";
+			fann_save(ann, "MyoCal.net");
+			std::cout << "User Calibration complete! \n\n";
+
 		}
 		else{
+			std::cout << "Calibrating with stored general data... \n\n"; 
 			fann_train_on_file(ann, "C:\\Users\\Ayotunde\\Documents\\Visual Studio 2013\\Projects\\fann-master\\fann-master\\examples\\Myo.data", max_epochs, epochs_between_reports, desired_error);
-			std::cout << "Calibrating with stored general data! \n\n";
+			fann_save(ann, "Myo.net");
+			std::cout << "Calibration complete! \n\n";
 		}
-		fann_save(ann, "Myo.net");
+		
 
 		fann_destroy(ann);
 	}
@@ -37,8 +42,12 @@ namespace Myo_Fann
 	{
 		// Uses/Tests the Neural Network 
 		fann_type *calc_out;
-
-		struct fann *ann = fann_create_from_file("Myo.net");
+		
+		struct fann *ann;
+		if (TerminatorMyo_Fann::toCalibrate)
+			ann = fann_create_from_file("MyoCal.net");
+		else
+			ann = fann_create_from_file("Myo.net");
 
 		calc_out = fann_run(ann, input);
 
